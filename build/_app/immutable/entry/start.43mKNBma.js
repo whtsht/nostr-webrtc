2a7d17b5d059a7c73d98056dc93a89778187ca53
whtsht <whiteshirt0079@gmail.com>
2024-09-05 18:30:45 +0900

    remove build directory

--- build/_app/immutable/entry/start.43mKNBma.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DyMlMRD8.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,11 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    })
+			fallback: '404.html'
+		}),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
 	}
 };
 
